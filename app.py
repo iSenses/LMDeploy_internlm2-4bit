4822b51fdb39cb3df332d1e5246f458068aa04df
nachtmusik <nachtmusik@163.com>
2024-05-03 15:26:01 +0800

    minor change

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import os
 from lmdeploy.serve.gradio.turbomind_coupled import run_local
 from lmdeploy import pipeline, TurbomindEngineConfig, ChatTemplateConfig
 
-backend_config = TurbomindEngineConfig(session_len=2048, cache_max_entry_count=0.2)
+backend_config = TurbomindEngineConfig(cache_max_entry_count=0.2)
 chat_template_config = ChatTemplateConfig(model_name='internlm2-chat-1_8b')
 
 base_path = './4bit-1_8b'
 os.system(f'git clone https://code.openxlab.org.cn/mingyanglee/internlm2-chat-1_8b-4bit.git {base_path}')
 
 
-run_local(base_path, model_name='internlm2-chat-1_8b', backend_config=backend_config, chat_template_config=chat_template_config, server_port=7860)
+run_local(base_path, model_name='internlm2-chat-1_8b', backend_config=backend_config, chat_template_config=chat_template_config, server_port=7860, tp=1)
